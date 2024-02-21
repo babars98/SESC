08bea713eb51577066b29ab84c2f1890e5326a90
Babar <babarshahzad0@gmail.com>
2024-02-21 21:40:22 +0000

    Week 4 Lab work and log book

---
+++ Week 3/MavenIntelliJ2024/src/main/java/uk/ac/leedsbeckett/App.java
@@ -0,0 +1,17 @@
+package uk.ac.leedsbeckett;
+
+import org.joda.time.LocalTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+        System.out.println( "Hello World!" );
+    }
+}
